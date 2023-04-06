b05caf68d448eb5e42907f970a28e75aaf57d7ea
AppleTest2 <appletest2@Appletests-MacBook-Air.local>
2023-04-06 11:06:23 +0200

    Initial commit

---
+++ src/main/java/com/example/Laboration2/Laboration2Application.java
@@ -0,0 +1,13 @@
+package com.example.Laboration2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboration2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboration2Application.class, args);
+	}
+
+}
