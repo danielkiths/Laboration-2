4ca741e33caa0e098aabeadddefe67a75a6d7ce1
AppleTest2 <appletest2@Appletests-MacBook-Air.local>
2023-04-06 15:38:43 +0200

    Update

--- src/test/java/com/example/Laboration2/Laboration2ApplicationTests.java
+++ src/test/java/com/example/Laboration2/Laboration2ApplicationTests.java
@@ -41,133 +41,128 @@ public class Laboration2ApplicationTests {
     @DisplayName("Kontrollera att webbplatsens titel stämmer")
     @Test
         void checkWebsiteTitle() {
-            String websiteTitle = driver.getTitle();
-            assertEquals("SVT Play", websiteTitle, "Titeln verkar inte stämma");
+        String websiteTitle = driver.getTitle();
+        assertEquals("SVT Play", websiteTitle, "Titeln verkar inte stämma");
     }
 
     @DisplayName("Kontrollera att webbplatsens logotyp är synlig")
     @Test
         void checkLogo() {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/header/div[2]/div/div/nav/a")));
-            WebElement logo = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/header/div[2]/div/div/nav/a"));
-            boolean result = logo.isDisplayed();
-            assertTrue(result, "Loggan verkar inte synas");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/header/div[2]/div/div/nav/a")));
+        WebElement logo = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/header/div[2]/div/div/nav/a"));
+        boolean result = logo.isDisplayed();
+        assertTrue(result, "Loggan verkar inte synas");
     }
 
     @DisplayName("Kontrollera att namnet på länken 'Start' stämmer")
     @Test
         void checkLinkText1() {
-            WebElement linkElement = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/header/div[2]/div/div/nav/ul/li[1]/a"));
-            String linkText = linkElement.getText();
-            assertEquals("START", linkText, "Länktexten verkar inte stämma");
+        WebElement linkElement = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/header/div[2]/div/div/nav/ul/li[1]/a"));
+        String linkText = linkElement.getText();
+        assertEquals("START", linkText, "Länktexten verkar inte stämma");
     }
 
     @DisplayName("Kontrollera att namnet på länken 'Program' stämmer")
     @Test
         void checkLinktext2() {
-            WebElement linkElement = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/header/div[2]/div/div/nav/ul/li[2]"));
-            String linkText = linkElement.getText();
-            assertEquals("PROGRAM", linkText, "Länktexten verkar inte stämma");
+        WebElement linkElement = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/header/div[2]/div/div/nav/ul/li[2]"));
+        String linkText = linkElement.getText();
+        assertEquals("PROGRAM", linkText, "Länktexten verkar inte stämma");
     }
 
     @DisplayName("Kontrollera att namnet på länken 'Kanaler' stämmer")
     @Test
         void checkLinktext3() {
-            WebElement linkElement = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/header/div[2]/div/div/nav/ul/li[3]"));
-            String linkText = linkElement.getText();
-            assertEquals("KANALER", linkText, "Länktexten verkar inte stämma");
+        WebElement linkElement = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/header/div[2]/div/div/nav/ul/li[3]"));
+        String linkText = linkElement.getText();
+        assertEquals("KANALER", linkText, "Länktexten verkar inte stämma");
     }
 
 
     @DisplayName("Kontrollera att länken för 'Tillgänglighet i SVT Play är synlig")
     @Test
         void checkFooterLInk() {
-            WebElement footerlink = driver.findElement(By.xpath("//a[@href=\"https://kontakt.svt.se/guide/tillganglighet\"]"));
-            boolean result = footerlink.isDisplayed();
-            assertTrue(result, "Länken verkar inte synas");
+        WebElement footerlink = driver.findElement(By.xpath("//a[@href=\"https://kontakt.svt.se/guide/tillganglighet\"]"));
+        boolean result = footerlink.isDisplayed();
+        assertTrue(result, "Länken verkar inte synas");
     }
 
     @DisplayName("Kontrollera att rätt länktext visas för länken 'Tillgänglighet i SVT Play'")
     @Test
         void checkFooterLinkText() {
-            WebElement footerlink = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/footer/div/div[5]/div[2]/p[1]/a/span[2]"));
-            String text = footerlink.getText();
-            assertEquals("Tillgänglighet i SVT Play", text, "Länkens text verkar inte stämma");
+        WebElement footerlink = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/footer/div/div[5]/div[2]/p[1]/a/span[2]"));
+        String text = footerlink.getText();
+        assertEquals("Tillgänglighet i SVT Play", text, "Länkens text verkar inte stämma");
     }
 
     @DisplayName("Följ länken 'Tillgänglighet i SVT Play' och kontrollera huvudrubriken")
     @Test
         void checkHeadLine() {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href=\"https://kontakt.svt.se/guide/tillganglighet\"]")));
-            driver.findElement(By.xpath("//a[@href=\"https://kontakt.svt.se/guide/tillganglighet\"]")).click();
-            String text = driver.findElement(By.className("text-3xl")).getText();
-            assertEquals("Så arbetar SVT med tillgänglighet", text, "Rubriken verkar inte stämma");
-            driver.navigate().back();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href=\"https://kontakt.svt.se/guide/tillganglighet\"]")));
+        driver.findElement(By.xpath("//a[@href=\"https://kontakt.svt.se/guide/tillganglighet\"]")).click();
+        String text = driver.findElement(By.className("text-3xl")).getText();
+        assertEquals("Så arbetar SVT med tillgänglighet", text, "Rubriken verkar inte stämma");
+        driver.navigate().back();
     }
 
     @DisplayName("Navigera till sidan 'Program' och kontrollera antalet kategorier som visas")
     @Test
         void checkNumberOfCategories() {
-            WebElement programElement = driver.findElement(By.xpath("//a[@href=\"/program\"]"));
-            programElement.click();
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("article.sc-a9073dc0-0")));
-            List<WebElement> categories = driver.findElements(By.cssSelector("article.sc-a9073dc0-0"));
-            assertEquals(17, categories.size(), "Antalet kategorier verkar inte stämma");
-            driver.navigate().back();
+        WebElement programElement = driver.findElement(By.xpath("//a[@href=\"/program\"]"));
+        programElement.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("article.sc-a9073dc0-0")));
+        List<WebElement> categories = driver.findElements(By.cssSelector("article.sc-a9073dc0-0"));
+        assertEquals(17, categories.size(), "Antalet kategorier verkar inte stämma");
+        driver.navigate().back();
     }
 
     @DisplayName("Kontrollera antalet stripes som visas i start sidan")
     @Test
         void checkNumberOfStripes() {
-            List<WebElement> stripes = driver.findElements(By.cssSelector("a.sc-f796aafd-4"));
-            assertEquals(33, stripes.size(), "Antalet stripes verkar inte stämma");
+        List<WebElement> stripes = driver.findElements(By.cssSelector("a.sc-f796aafd-4"));
+        assertEquals(33, stripes.size(), "Antalet stripes verkar inte stämma");
     }
 
     @DisplayName("Kontrollera titeln i sidan 'Kanaler'")
     @Test
         void checkTitleInKanaler() {
-            driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/header/div[2]/div/div/nav/ul/li[3]/a")).click();
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"play_main-content\"]/div/h1")));
-            String title = driver.findElement(By.cssSelector("h1.sc-c358b5df-0")).getText();
-            assertEquals("På SVT just nu", title, "Rubriken i Kanaler verkar inte stämma");
-            driver.navigate().back();
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/header/div[2]/div/div/nav/ul/li[3]/a")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"play_main-content\"]/div/h1")));
+        String title = driver.findElement(By.cssSelector("h1.sc-c358b5df-0")).getText();
+        assertEquals("På SVT just nu", title, "Rubriken i Kanaler verkar inte stämma");
+        driver.navigate().back();
     }
 
     @DisplayName("Kontrollera att länken för dataanvändning visas i 'Inställningar' sidan")
     @Test
         void checkLinkInSettings() {
-            driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/footer/div/div[3]/a[1]")).click();
-            WebElement link = driver.findElement(By.xpath("//*[@id=\"play_main-content\"]/div/div[1]/div/div/p[2]/a"));
-            boolean result = link.isDisplayed();
-            assertTrue(result, "Länken verkar inte synas");
-            driver.navigate().back();
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/footer/div/div[3]/a[1]")).click();
+        WebElement link = driver.findElement(By.xpath("//*[@id=\"play_main-content\"]/div/div[1]/div/div/p[2]/a"));
+        boolean result = link.isDisplayed();
+        assertTrue(result, "Länken verkar inte synas");
+        driver.navigate().back();
     }
 
     @DisplayName("Kontrollera antal genvägar i 'Kontakt' sidan")
     @Test
         void checkNumberOfShortcuts() {
-            driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/footer/div/div[3]/a[2]")).click();
-            List<WebElement> shortcuts = driver.findElements(By.cssSelector("a.py-6"));
-            assertEquals(5, shortcuts.size(), "Antalet genvägar verkar inte stämma");
-            driver.navigate().back();
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[3]/div/footer/div/div[3]/a[2]")).click();
+        List<WebElement> shortcuts = driver.findElements(By.cssSelector("a.py-6"));
+        assertEquals(5, shortcuts.size(), "Antalet genvägar verkar inte stämma");
+        driver.navigate().back();
     }
 
     @DisplayName("Kontrollera att SVT loggan i sidfoten är synlig")
     @Test
         void checkLogotypeInFooter() {
-            WebElement logoType = driver.findElement(By.className("sc-87f10045-1"));
-            boolean result = logoType.isDisplayed();
-            assertTrue(result, "Loggan i sidfoten syns inte");
+        WebElement logoType = driver.findElement(By.className("sc-87f10045-1"));
+        boolean result = logoType.isDisplayed();
+        assertTrue(result, "Loggan i sidfoten syns inte");
 
-    }
-
-    @AfterAll
-        static void tearDown() {
-            driver.quit();
     }
 
 }
